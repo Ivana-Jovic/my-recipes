59d3acc716a0ed760fe87173ad54ada05346b1e4
Ivana-Jovic <ivana.n.jovic@gmail.com>
2023-08-24 14:16:34 +0200

    feat: added useInfiniteQuery instead of useQuery

--- screens/Recipes.tsx
+++ screens/Recipes.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { FlatList, View, StyleSheet } from "react-native";
-import { QueryFunctionContext, UseQueryResult, useQuery } from "react-query";
+import {
+  QueryFunctionContext,
+  UseQueryResult,
+  useQuery,
+  useInfiniteQuery,
+  UseInfiniteQueryResult,
+} from "react-query";
 import { useStore } from "../store/store";
 //Components
 import RecipeCard from "../components/RecipeCard";
@@ -11,33 +17,74 @@ import { Colors } from "../utils/colors";
 import { RecipeType } from "../utils/types";
 
 const fetchRecipes: (
-  context: QueryFunctionContext<[string, number]>,
-) => Promise<RecipeType[]> = async (context) => {
-  const page = context.queryKey[1];
+  page: number,
+  // context: QueryFunctionContext<[string, number]>,
+) => Promise<RecipeType[]> = async (
+  page,
+  // context
+) => {
+  // const page = context.queryKey[1];
   const response = await fetch(`http://localhost:3000/recipes/?_page=${page}`); // TODO: ne radi na androidu
   const jsonData = (await response.json()) as RecipeType[];
+  console.log(page);
   return jsonData;
 };
 
 function Recipes() {
-  const [page, setPage] = useState<number>(1);
+  // const [page, setPage] = useState<number>(1);
   const [pageChanged, setPageChanged] = useState<boolean>(false);
 
   const recipesContext = useStore((state) => state.recipes);
   const addRecipes = useStore((state) => state.addRecipes);
 
+  // const {
+  //   data: recipes,
+  //   isLoading,
+  //   isError,
+  //   refetch,
+  //   hasNextPage,
+  //   fetchNextPage,
+  //   isFetchingNextPage,
+  // }: UseQueryResult<RecipeType[], [string, number]> =
+  // useQuery(
+  //   ["recipes", page],
+  //   fetchRecipes,
+  //   {
+  //     onSuccess(data) {
+  //   if (recipesContext.length === 0 || pageChanged) {
+  //     addRecipes(data);
+  //   }
+  // },
+  //   },
+  // );
   const {
     data: recipes,
     isLoading,
     isError,
-  }: UseQueryResult<RecipeType[], [string, number]> = useQuery(
-    ["recipes", page],
-    fetchRecipes,
+    refetch,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  }: UseInfiniteQueryResult<RecipeType[], [string, number]> = useInfiniteQuery(
+    "todos",
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    ({ pageParam = 1 }) => fetchRecipes(pageParam),
     {
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage =
+          lastPage.length === 10 ? allPages.length + 1 : undefined;
+        return nextPage;
+      },
       onSuccess(data) {
-        if (recipesContext.length === 0 || pageChanged) {
-          addRecipes(data);
-        }
+        console.log("okk");
+        addRecipes(data.pages[data.pages.length - 1]);
+        console.log(
+          " addRecipes(data.pages[data.pages.length - 1])",
+          data.pages[0].length,
+        );
+        data.pages[data.pages.length - 1].forEach((element) => {
+          console.log(-element.id);
+        });
       },
     },
   );
@@ -60,15 +107,23 @@ function Recipes() {
           keyExtractor={(item) => item.id.toString()}
           onEndReached={() => {
             console.log("before setPageChanged(true);");
-            if (recipes?.length !== 0) {
-              setPage((prev) => {
-                return prev + 1;
-              });
-              setPageChanged(true);
-              console.log("setPageChanged(true);");
+            // if (recipes?.length !== 0) {
+            //   // setPage((prev) => {
+            //   //   return prev + 1;
+            //   // });
+
+            //   setPageChanged(true);
+            //   console.log("setPageChanged(true);");r
+            // }
+            if (hasNextPage) {
+              fetchNextPage()
+                .then(() => {
+                  console.log("hasNextPage-fetchNextPage");
+                })
+                .catch(() => {});
             }
           }}
-          onEndReachedThreshold={0.1} // todo koja vrednost je ok ovde
+          onEndReachedThreshold={0.5} // todo koja vrednost je ok ovde
         />
       </View>
     </View>
